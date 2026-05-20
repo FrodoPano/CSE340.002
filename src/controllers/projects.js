99e06c219cea7f6566e672c99f74590684f344fc
FrodoPano <frodopano@gmail.com>
2026-05-20 10:29:10 -0600

    Team Activity

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,13 +1,25 @@
 // Import any needed model functions
-import { getAllProjects } from '../models/projects.js';
+import { getAllProjects, getUpcomingProjects, getProjectDetails } from '../models/projects.js';
+
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
 
 // Define any controller functions
 const showProjectsPage = async (req, res) => {
-    const projects = await getAllProjects();
-    const title = 'Service Projects';
-
+    const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
+    const title = 'Upcoming Service Projects';
+    
+    console.log('Retrieved projects:', projects);
+    
     res.render('projects', { title, projects });
-};  
+};
+
+const showProjectDetailsPage = async (req, res) => {
+    const projectId = req.params.id;
+    const project = await getProjectDetails(projectId);
+    const title = project ? project.title : 'Project Not Found';
+    
+    res.render('project', { title, project });
+}; 
 
 // Export any controller functions
-export { showProjectsPage };
+export { showProjectsPage, showProjectDetailsPage };
